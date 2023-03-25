882e48935ba727c746b1f45dac1c4d96b1e68acc
lexhazred <electric@valentine.anonaddy.com>
2023-03-25 02:59:14 -0400

    linear-tensor-memory.py

---
+++ linear-tensor-memory.py
@@ -0,0 +1,4 @@
+# This script simulates Linear Tensor Memory, see the readme for description.
+# Assumptions with this script regarding cost are the same with others:
+# Initial Prompt Tokens = 400
+# Message Tokens = 100
